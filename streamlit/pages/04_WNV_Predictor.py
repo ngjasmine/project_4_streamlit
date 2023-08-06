39a9f8311e73d260192887e53562abec8cfcaed2
ngjasmine <nwzjasmine@gmail.com>
2023-08-06 20:11:57 +0800

    update 04_WNV_Predictor.py

--- streamlit/pages/04_WNV_Predictor.py
+++ streamlit/pages/04_WNV_Predictor.py
@@ -57,16 +57,18 @@ def get_predictors():
     
 
     # User input
-    street = st.sidebar.selectbox('Street', sorted(df_filtered['street'].unique()), index=0) # index is the default
-    block = st.sidebar.slider('Block Number', float(df_filtered['block'].min()), float(df_filtered['block'].max()), step=1.0, value=70.0) # value is the default
+    address = st.sidebar.selectbox('Address', sorted(df_filtered['address_number_and_street'].unique()), index=0)
+    
 
-    #Get latitude and longitude from dataframe based on user input for street and block
-    filtered_df = df[(df['block']==block) & (df['street']==street)]
+    #Get latitude and longitude, street and block from dataframe based on user input for address
+    filtered_df = df[df['address_number_and_street']==address]
     if len(filtered_df) > 0:
         latitude = filtered_df['latitude'].mean()
         longitude = filtered_df['longitude'].mean()
-    else:
-        st.error('Did you input the correct address?')
+        # Access street name for first row 
+        street = filtered_df['street'].iloc[0]
+        block = filtered_df['block'].mean()
+
 
     # latitude = st.sidebar.slider('Latitude',  float(df_filtered['latitude'].min()), float(df_filtered['latitude'].max()), float(df_filtered['latitude'].min()), key='3')
     # longitude = st.sidebar.slider('Longitude', float(df_filtered['longitude'].min()), float(df_filtered['longitude'].max()),float(df_filtered['longitude'].min()), key='4')
